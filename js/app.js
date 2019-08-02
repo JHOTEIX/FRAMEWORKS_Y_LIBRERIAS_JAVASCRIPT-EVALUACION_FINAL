e4a067cc64942b1d152d0b42b21d2d2df94675ca
JHONNATHAN TEIXEIRA <jhonnathan.teixeira@gmail.com>
2019-08-02 09:26:35 -0400

    v0.1
    Inicio del Proyecto

---
+++ js/app.js
@@ -0,0 +1,13 @@
+//1 - Cambiar el Color Del Titulo y Alternarlo Indefenidamente con Dos Colores
+setInterval(function(){ 
+	$('.main-titulo').css('color',' #fff');  // Alterna la Clase cada 2 Segundos 
+	setTimeout(function(){
+		$('.main-titulo').css('color',' #DCFF0E'); // Devuelve la Clase cada 1 Segundos 
+	},500)
+ },1000);
+
+//2. Funcion para generar Dulces Aleatoriamente
+
+
+
+
