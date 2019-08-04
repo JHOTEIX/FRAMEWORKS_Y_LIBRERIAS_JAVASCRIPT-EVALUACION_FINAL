1aebd0f67bf861f862016d6840d9be5cd015f0bc
JHONNATHAN TEIXEIRA <jhonnathan.teixeira@gmail.com>
2019-08-04 15:12:35 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,8 +6,14 @@ setInterval(function(){
 	},500)
  },1000);
 
-//2. Funcion para generar Dulces Aleatoriamente
-
-
-
-
+//2 - Funcion para generar Dulces Aleatoriamente y insertalos en la tabla
+var arrays_dulces=[];
+for(c=1;c<=7;c++){
+	for (f=0;f<=5;f++){
+		var dulces_random=Math.floor((Math.random()*4)+1);
+		$('.col-'+c).append('<img src="image/' + dulces_random + '.png" class="element"></img>');
+		arrays_dulces.push(dulces_random);
+		$('.col-'+c).draggable();
+		console.log('[' + c + ',' + f + '] = ' + arrays_dulces[c,f]);
+	}
+}
